90efe3d5a45b0123dd3c6fb6d98f5b1cd56ea421
Ruben Darío Rueda <ingeniero.rubenrueda@gmail.com>
2023-07-25 18:40:03 -0500

    FirstCommit
    add propertys from selection to CollectionView Control.

---
+++ Views/CollectionView-Vista.xaml.cs
@@ -0,0 +1,25 @@
+using ControlesTipoVista.Data;
+
+namespace ControlesTipoVista.Views;
+
+public partial class CollectionView_Vista : ContentPage
+{
+	public CollectionView_Vista()
+	{
+		InitializeComponent();
+	}
+
+    private void CollectionView_RemainingItemsThresholdReached(object sender, EventArgs e)
+    {
+
+    }
+
+    private void CollectionView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        //string Anterior = (e.PreviousSelection.FirstOrDefault() as Robot)?.Nombre;
+        //string Actual = (e.CurrentSelection.FirstOrDefault() as Robot)?.Nombre;
+
+        //lbl1.Text = Anterior;
+        //lbl2.Text = Actual;
+    }
+}
